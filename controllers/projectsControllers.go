5d9cb5763197ebe207f7614db8534f683890e875
aManNamedTed <davidamante17@yahoo.com>
2019-09-29 14:09:09 -0700

    core function done, time to connect db and test

---
+++ controllers/projectsControllers.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+	"go-api/models"
+	"encoding/json"
+	u "go-api/utils"
+)
+
+var GetProjectsFor = func(w http.ResponseWriter, r *http.Request) {
+	data := models.GetProjects()
+	resp := u.Message(true, "success")
+	resp["data"] = data
+	u.Respond(w, resp)
+}
---
+++ models/project.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	u "go-api/utils"
+	"github.com/jinzhu/gorm"
+	"fmt"
+)
+
+type Project struct {
+	gorm.Model
+	ProjectId uint `json:"id"`
+	Title string `json:"title"`
+	Link string `json:"link"`
+	Date string `json:"date"`
+	Description string `json:"description"`
+	Technologies []string `json:"technologies"`
+	Categories []string `json:"categories"`
+}
+
+func GetProject(id uint) (*Project) {
+	project := &Project{}
+	err := GetDB().Table("projects").Where("id = ?", id).First(project).Error
+	if err != nil {
+		return nil
+	}
+	
+	return project
+}
+
+func GetProjects() ([]*Project) {
+	projects := make([]*Project, 0)
+	err := GetDB().Table("projects").Find(&projects).Error
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return projects
+}
